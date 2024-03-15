a395c0d7dc76a7e6f2461af1d7cb4621ea4c82ea
umamanasa <umamanasa49@gmail.com>
2024-03-15 14:52:40 +0530

    shell-script example

--- login.sh
+++ login.sh
@@ -1,2 +1,16 @@
+#To print a message
 echo Hello World!!
 echo Good Morning, Have a nice day!
+
+#To print a message with colors
+# echo -e "\e[CODEmMESSAGE\e[0m"
+# Red       31
+# Green     32
+# Yellow    33
+# Blue      34
+# Magenta   35
+# Cyan      36
+
+echo -e "\e[33mHello\e[0m"
+echo -e "\e[36mHello\e[0m"
+echo -e "\e[32mHello\e[0m"
