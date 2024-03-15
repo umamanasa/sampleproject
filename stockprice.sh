06d223fc68690d701ec5a74cf913e6a471e350ca
umamanasa <umamanasa49@gmail.com>
2024-03-15 16:12:57 +0530

    shell-script example

--- stockprice.sh
+++ stockprice.sh
@@ -1,6 +1,7 @@
-curl -s $1 | grep nsecp |awk -F '[<>]' '{print $3}'
 
 function stock_price() {
-  curl -s "https://www.nseindia.com/market-data/live-equity-market?symbol=NIFTY%2050"
+  curl -s "https://www.moneycontrol.com/india/stockpricequote/auto-lcvshcvs/tatamotors/TM03"
 }
+alias stockprice=stock_price
+
 echo stock_price
