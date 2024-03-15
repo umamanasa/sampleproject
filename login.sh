a03f92bc6494ce4cf4462552bfd2f165a7a3f17c
umamanasa <umamanasa49@gmail.com>
2024-03-15 17:12:44 +0530

    login example

--- login.sh
+++ login.sh
@@ -1,43 +1,21 @@
-#To print a message
-echo Good Morning, Have a nice day!
+#shell script for login
+#inputs
+valid_username= "manasa"
+valid_password= "manasa123"
 
-#To print a message with colors
-# echo -e "\e[CODEmMESSAGE\e[0m"
-# Red       31
-# Green     32
-# Yellow    33
-# Blue      34
-# Magenta   35
-# Cyan      36
+#functional logic
+func_login() {
+  echo "Enter Username: "
+  read username
+  echo "Enter Password: "
+  read -s password
 
-echo -e "\e[33mHello\e[0m"
-
-#Variables
-a=10
-echo a is $a
-
-DATE=$(date)   #date variable
-echo Today date is $DATE
-
-ADD=$(( 2+2 ))   #special variables
-echo add of 2+2 = $ADD
-
-#Conditional statements
-#if [ condition ]
-#then
-#  statements
-#  fi
-x=10
-y=12
-if [ $x -eq $y ]
-then
-  echo "EQUAL"
-  else
-    echo "NOT EQUAL"
-  fi
-
-#while condition
-#while [ condition ]
-#do
-#  statements
-#  done
+  if [ "$username" == "$valid_username"  ] && [ "$password" == "$valid_password" ]
+  then
+    echo "Login Successful!"
+    else
+      echo "Login Unsuccessful!"
+      echo "please try again!!"
+      fi
+}
+func_login
