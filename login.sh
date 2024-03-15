a486f747db098c6262af26adf578a36de9b40f4c
umamanasa <umamanasa49@gmail.com>
2024-03-15 17:19:54 +0530

    login example

--- login.sh
+++ login.sh
@@ -1,16 +1,17 @@
 #shell script for login
 #inputs
-valid_username= "manasa"
-valid_password= "manasa123"
+valid_username="manasa"
+valid_password="manasa123"
 
 #functional logic
 func_login() {
   echo "Enter Username: "
   read username
+
   echo "Enter Password: "
   read -s password
 
-  if [ "$username" == "$valid_username"  ] && [ "$password" == "$valid_password" ]
+  if [ "$username" == "$valid_username"  ] && [ "$password" == "$valid_password" ];
   then
     echo "Login Successful!"
     else
