11acba38224420ad18e299c0ec5947f105320eb7
umamanasa <umamanasa49@gmail.com>
2024-06-24 17:30:02 +0530

    shell added

--- shell.sh
+++ shell.sh
@@ -3,3 +3,14 @@ echo "No of arguments: $#"
 echo "Name of the Script: $0"
 echo "First Argument: $1"
 echo "Second Argument: $2"
+
+# storing output of commands
+# syntax 1
+#var=$(linux command )
+# syntax 2
+#var2='linux command'
+
+# Operations on a file
+c = 'cat $file | wc -c'
+w = 'cat $file | wc -w'
+l=`grep -c "." $file`
